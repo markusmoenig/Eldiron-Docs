5b492985e3b2d6b4cd9e22196db68f19fe724b01
Markus Moenig <markus@moenig.io>
2025-04-21 16:52:33 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
   organizationName: "markusmoenig", // Usually your GitHub org/user name.
   projectName: "Eldiron-Docs", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
